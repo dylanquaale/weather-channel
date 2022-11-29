3c6c0b4e42bbcd9f52bb02072afc60c0abb3dcf5
dylanquaale <dylanquaale33@gmail.com>
2022-11-29 11:31:01 -0600

    fetch add

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,19 @@
 var apiKey = "e2e9e2de69bdf29accc1e83caa611a00";
 var city;
-
+var responseText = document.getElementById('response-text');
 
 var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
+
+function getApi(requestUrl) {
+    fetch(queryUrl)
+      .then(function (response) {
+        console.log(response);
+        if (response.status === 200) {
+          responseText.textContent = response.status;
+        }
+        return response.json();
+    });
+  }
+  
+  getApi(queryUrl);
