6df0b16c1dfb5574135759db730016c7b11db8e1
dylanquaale <dylanquaale33@gmail.com>
2022-11-29 17:22:28 -0600

    adding dates

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,7 @@ var key = 'e2e9e2de69bdf29accc1e83caa611a00';
 var city = 'Minneapolis';
 
 
+var date = dayjs().format('dddd, MMMM Do YYYY');
+var datetime = dayjs()('YYYY-MM-DD HH:MM:SS')
 
+var cityHist = [];
