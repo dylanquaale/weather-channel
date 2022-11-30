dea9ee6ea7b4606bfb0120bc7a8ab9fc3749fe99
dylanquaale <dylanquaale33@gmail.com>
2022-11-29 18:16:47 -0600

    fixed dayjs was missing a period on line 6

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,11 @@ var city = 'Minneapolis';
 
 
 var date = dayjs().format('dddd, MMMM Do YYYY');
-var datetime = dayjs()('YYYY-MM-DD HH:MM:SS')
-
+var dateTime = dayjs().format('YYYY-MM-DD HH:MM:SS')
+console.log('dayjs');
 var cityHist = [];
+
+
 //saves text value of each search as an array in storage
 $('.search').on("click", function (event){
     event.preventDefault();
@@ -20,3 +22,31 @@ $('.search').on("click", function (event){
     getHistory();
     getWeatherToday();
 });
+
+//this will create buttons for the search history
+var constHistEl = $('.cityHist');
+function getHistory() {
+    constHistEl.empty();
+
+    for (let i = 0; i < cityHist.length; i++) {
+        var rowEl= $('<row>');
+        var btnEl = $('<button>').text(`${cityHist[i]}`)
+
+        rowEl.addClass('row hisBtnRow');
+        btnEl.addClass('btn btn-outline-seconday histBtn');
+        btnEl.attr('type', 'button');
+
+        constHistEl.prepend(rowEl);
+        rowEl.append(btnEl);
+    } if (!city) {
+        return;
+    }
+    $('histBtn').on("click", function (event){
+        event.preventDefault();
+        city = $(this).text();
+        fiveForecastEl.empty();
+        getWeatherToday();    
+
+    
+    });
+};
